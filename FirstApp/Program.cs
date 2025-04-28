6744db6edbdaa9c8aa9580dcbe32501cc6b42e37
2024ashikul <2024ashikul@gmail.com>
2025-04-28 11:30:00 +0600

    Doing Lab assignment 2 which has a good C++ PROJECT

---
+++ FirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+namespace first{
+class Print{
+    static void printName(string firstname,string secondname){
+        Console.WriteLine(firstname + " " + secondname);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hi there");
+        Print.printName("ashikul","islam");
+
+    }
+    
+}
+}
